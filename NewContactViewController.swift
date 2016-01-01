b858f766c72427f87a9661f82d6bea551d67d8b3
turboflanagan <turboflanagan@gmail.com>
2016-01-01 16:41:59 -0500

    Adjust the New Contact View Controller to Handle Editing

--- NewContactViewController.swift
+++ NewContactViewController.swift
@@ -13,6 +13,14 @@ class NewContactViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let contactId = self.editContactId {
+            self.editedContact = DataManager.sharedManager.getContact(contactId : contactId)
+            
+            if self.editedContact != nil {
+                self.updateTextFields()
+            }
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -40,23 +48,68 @@ class NewContactViewController: UIViewController {
         
         DataManager.sharedManager.save()
         
-        if self.delegate != nil {
-            
-            let newContact = DataManager.sharedManager.createContact()
-            
-            newContact.firstName = self.firstNameTextField.text
-            newContact.lastName = self.lastNameTextField.text
-            newContact.phoneNumber = self.phoneNumberTextField.text
-            newContact.address?.street = self.streetAddressTextField.text
-            newContact.address?.city = self.cityTextField.text
-            newContact.address?.state = self.stateTextField.text
-            newContact.address?.zipCode = self.zipCodeTextField.text
-            
-            self.delegate?.didCreateNewContact(newContact)
-            
+//        if self.delegate != nil {
+//            
+//            let newContact = DataManager.sharedManager.createContact()
+//            
+//            newContact.firstName = self.firstNameTextField.text
+//            newContact.lastName = self.lastNameTextField.text
+//            newContact.phoneNumber = self.phoneNumberTextField.text
+//            newContact.address?.street = self.streetAddressTextField.text
+//            newContact.address?.city = self.cityTextField.text
+//            newContact.address?.state = self.stateTextField.text
+//            newContact.address?.zipCode = self.zipCodeTextField.text
+//            
+//            self.delegate?.didCreateNewContact(newContact)
+//            
+//        }
+        
+        var contact : Contact! = nil
+        
+        if self.editedContact != nil {
+            contact = self.editedContact
+        }
+        else {
+            contact = DataManager.sharedManager.createContact()
         }
         
+        self.updateContact(contact)
+        
         self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    var editContactId : String?
+    private var editedContact : Contact?
+    
+    func updateTextFields() {
+        self.firstNameTextField.text = self.editedContact?.firstName
+        self.lastNameTextField.text = self.editedContact?.lastName
+        self.phoneNumberTextField.text = self.editedContact?.phoneNumber
+        self.streetAddressTextField.text = self.editedContact?.address?.street
+        self.cityTextField.text = self.editedContact?.address?.city
+        self.stateTextField.text = self.editedContact?.address?.state
+        self.zipCodeTextField.text = self.editedContact?.address?.zipCode
+    }
+    
+    func updateContact(contact: Contact) {
+        contact.firstName = self.firstNameTextField.text
+        contact.lastName = self.lastNameTextField.text
+        contact.phoneNumber = self.phoneNumberTextField.text
+        contact.address?.street = self.streetAddressTextField.text
+        contact.address?.city = self.cityTextField.text
+        contact.address?.state = self.stateTextField.text
+        contact.address?.zipCode = self.zipCodeTextField.text
+        
+        DataManager.sharedManager.save()
+        
+        if self.delegate != nil {
+            if self.editedContact != nil {
+                self.delegate?.didUpdateContact(contact)
+            }
+            else {
+            self.delegate!.didCreateNewContact(contact)
+            }
+        }
     }
 
     /*
